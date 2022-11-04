b7e28c2d9c06bf93a4ad051add3c9af42bd4ad85
miszczu1902 <stary2111@gmail.com>
2022-11-04 09:50:42 +0100

    MongoDB's connection finally works

---
+++ src/main/java/mongo/AbstractEntityMgd.java
@@ -0,0 +1,17 @@
+//package mongo;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//import org.bson.codecs.pojo.annotations.BsonProperty;
+//
+//import java.io.Serializable;
+//
+//@Data
+//@EqualsAndHashCode
+//@AllArgsConstructor
+//public class AbstractEntityMgd implements Serializable {
+//
+//    @BsonProperty("_id")
+//    private final UniqueIDMgd entityId;
+//}
---
+++ src/main/java/mongo/ClientEmbeddedMgd.java
@@ -0,0 +1,11 @@
+package mongo;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class ClientEmbeddedMgd extends AbstractEntityMgd {
+}
