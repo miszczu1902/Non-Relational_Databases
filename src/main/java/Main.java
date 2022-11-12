6b780331ba3796b9ccd1e5158126adb8ad9d7f55
miszczu1902 <stary2111@gmail.com>
2022-11-12 21:27:13 +0100

    MongoDB connection maybe works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,13 @@ import repositories.ClientRepository;
 
 public class Main {
     public static void main(String[] args) {
-        ClientRepository clientRepository = new ClientRepository();
-        clientRepository.initDbConnection();
-        clientRepository.add(new Client("ach", "to", "NBD", new Address(RandomUtils.nextLong(), "ll", "ll", "ll")));
-        System.out.println("Hello world");
+        try (ClientRepository clientRepository = new ClientRepository()) {
+            clientRepository.initDbConnection();
+            clientRepository.add(new Client("ach", "to", "NBD",
+                    new Address(RandomUtils.nextLong(), "ll", "ll", "ll")));
+            System.out.println("Hello world");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
