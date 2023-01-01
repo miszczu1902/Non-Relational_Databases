b14a76ee72afbc03f76306722ffec55fa7167bfd
miszczu1902 <stary2111@gmail.com>
2023-01-01 16:56:02 +0100

    Problem with resolving domain names ended

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,30 @@
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.dataformat.avro.AvroMapper;
+import com.fasterxml.jackson.dataformat.avro.AvroSchema;
 import kafka.producers.HotelProducer;
+import model.Client;
+import model.Reservation;
+import model.Room;
+import mongo.UniqueIdMgd;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        HotelProducer.initProducer();
+    public static void main(String[] args) throws ExecutionException, InterruptedException, JsonMappingException {
+        AvroMapper avroMapper = new AvroMapper();
+        AvroSchema schema = avroMapper.schemaFor(Reservation.class);
+        System.out.println(schema.getAvroSchema());
+
+        HotelProducer hotelProducer = new HotelProducer();
+        hotelProducer.send(new Reservation(
+                new UniqueIdMgd(UUID.randomUUID()),
+                new Room(),
+                LocalDateTime.now().toString(),
+                LocalDateTime.now().plusDays(1).toString(),
+                new Client(),
+                0));
+
     }
 }
--- src/main/java/kafka/producers/HotelProducer.java
+++ src/main/java/kafka/producers/HotelProducer.java
@@ -1,18 +1,23 @@
 package kafka.producers;
 
+import model.Reservation;
+import org.apache.commons.math3.util.Pair;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.UUIDSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class HotelProducer {
 
-    public static KafkaProducer PRODUCER;
+    public static final Pair<String, String> CLIENT_TOPIC = new Pair<>("reservations", "ZZpdX2DbSfW5T3HYNSeEdQ");
+    private KafkaProducer<UUID, Reservation> producer;
 
-    public static void initProducer() throws ExecutionException, InterruptedException {
+    public HotelProducer() throws ExecutionException, InterruptedException {
         Properties producerConfig = new Properties();
         producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 UUIDSerializer.class.getName());
@@ -20,9 +25,17 @@ public class HotelProducer {
                 StringSerializer.class.getName());
         producerConfig.put(ProducerConfig.CLIENT_ID_CONFIG, "local");
         producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9192,localhost:9292,localhost:9392");
+                "kafka1:9192,kafka2:9292,kafka3:9392");
         producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
 //        producerConfig.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
-        PRODUCER = new KafkaProducer(producerConfig);
+        this.producer = new KafkaProducer<>(producerConfig);
+    }
+
+    public void send(Reservation reservation) throws ExecutionException, InterruptedException {
+//        System.out.println(jsonObject.toString());
+        ProducerRecord<UUID, Reservation> record = new ProducerRecord<>(CLIENT_TOPIC.getKey(),
+                reservation.getId().getUuid(), reservation);
+
+        producer.send(record);
     }
 }
